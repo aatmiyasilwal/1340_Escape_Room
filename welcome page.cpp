0e4833fe7cadf69134bdfdd6c896f3d59015e62d
uddashya <125728691+uddashya@users.noreply.github.com>
2023-05-04 22:40:52 +0800

    Start page code

---
+++ welcome page.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <chrono> // for sleep function
+#include <thread> // for sleep function
+#include <string>
+using namespace std;
+
+int main() {
+    vector<string> message;
+    message.push_back("| ___ \\              | |             | |  ");
+    message.push_back("| |_/ /_ __ ___  __ _| | _____  _   _| |_ ");
+    message.push_back("| ___ \\ '__/ _ \\/ _` | |/ / _ \\| | | | __|");
+    message.push_back("| |_/ / | |  __/ (_| |   < (_) | |_| | |_ ");
+    message.push_back("\\____/|_|  \\___|\\__,_|_|\\_\\___/ \\__,_|\\__|");
+    
+    for (int i = 0; i < 5; i++) {
+        for (char c : message[i]) {
+            cout << c << flush;
+            this_thread::sleep_for(chrono::milliseconds(30)); // adjust delay time here
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
