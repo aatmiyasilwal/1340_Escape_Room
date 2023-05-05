19a6f4b1dfb83913657dd9d02ddd0aa6c5fb35b0
Aatmiya Silwal <63785496+aatmiyasilwal@users.noreply.github.com>
2023-05-05 11:35:08 +0800

    Fixed critical segmentation errors

--- triviaReader.cpp
+++ triviaReader.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main() {
     vector<string> questions;
-    vector<vector<string> > options;
+    vector<vector<string> > options(5, vector<string>(4, ""));
     vector<string> answers;
     string ques_number, text, line;
     int line_count = 0, question_count = 0, inner_count = 0;
@@ -21,7 +21,7 @@ int main() {
     while(fin >> ques_number){
         getline(fin, line);
         if (line_count % 6 == 0){
-            questions[question_count] = line;
+            questions.push_back(line);
             inner_count = 0;
         }
         else if (line_count % 6 >= 1 && line_count % 6 <= 4){
@@ -29,7 +29,7 @@ int main() {
             inner_count++;
         }
         else{
-            answers[question_count] = line;
+            answers.push_back(line);
             question_count++;
         }
         line_count++;
