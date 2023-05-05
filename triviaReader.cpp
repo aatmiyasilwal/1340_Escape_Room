cd9f88931572a79ad9f7d3bc6d3b425917bb1831
Aatmiya Silwal <63785496+aatmiyasilwal@users.noreply.github.com>
2023-05-05 10:45:31 +0800

    C++ file that reads the trivia.txt files

---
+++ triviaReader.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+
+int main() {
+    vector<string> questions;
+    vector<vector<string> > options;
+    vector<string> answers;
+    string ques_number, text, line;
+    int line_count = 0, question_count = 0, inner_count = 0;
+    ifstream fin;
+    ofstream fout;
+    
+    fin.open("trivia2.txt");
+
+    while(fin >> ques_number){
+        getline(fin, line);
+        if (line_count % 6 == 0){
+            questions[question_count] = line;
+            inner_count = 0;
+        }
+        else if (line_count % 6 >= 1 && line_count % 6 <= 4){
+            options[question_count][inner_count] = line;
+            inner_count++;
+        }
+        else{
+            answers[question_count] = line;
+            question_count++;
+        }
+        line_count++;
+    }
+    
+    fin.close();
+
+	return 0;
+}
