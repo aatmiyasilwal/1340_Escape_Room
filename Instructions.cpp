d613bb04c34babbb538f73576054f57428380d92
Siddhanth Shetty <121976233+shettysiddhanth@users.noreply.github.com>
2023-05-06 16:08:17 +0800

    added new instructions

--- Instructions.cpp
+++ Instructions.cpp
@@ -11,7 +11,9 @@ int main() {
     instructions.push_back("INSTRUCTIONS:\n");
     instructions.push_back("- BREAKOUT is a game where your goal is to BREAKOUT from the lobby.\n");
     instructions.push_back("- However, the lobby has 4 locks and each lock will unlock only once you complete tasks in each of the 4 rooms.\n");
+    instructions.push_back("- Use arrow keys to move & press 'a' to interact with doors in the main lobby and items within rooms.\n");
     instructions.push_back("- Now, enter your room of choice. However, think carefully because you cannot go back to previous rooms!\n");
+    instructions.push_back("- Interacting with room doors will promt you with a loading page, press 'ENTER' to continue.\n");
     instructions.push_back("- Every room has 2-3 mini-games and a unique word that unlocks the door.\n");
     instructions.push_back("- Complete all mini-games in a room to receive the letters that make up the unique word.\n");
     instructions.push_back("- Make a meaningful word out of the letters to unlock the door and escape the room.\n");
